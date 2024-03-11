75077a4da0e435f160a0ec2740bdf7dd84098579
Isaac Kao <tallfat@gmail.com>
2024-03-11 15:24:55 +0800

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Isaac Kao on 2024/3/11.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
